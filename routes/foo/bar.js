7bccabdcea0e7344771399f60d3eadeeffd93f15
Alexandre Santos <ajvsms@gmail.com>
2021-01-13 23:11:23 +0000

    Postgres inserts

--- routes/foo/bar.js
+++ routes/foo/bar.js
@@ -1,11 +1,37 @@
+// import fastify from 'fastify'
+import S from 'fluent-schema'
+
 export default async function (app, opts) {
   app.get('/', async () => {
     return 'foo bar'
   })
-  app.get('/user/', async () => {
+  app.get('/user/:id', async (req, res) => {
     const client = await app.pg.connect()
-    const { rows } = await client.query('SELECT now()')
+    const { rows } = await client.query('SELECT * from users where id = $1', [req.params.id])
     client.release()
-    return rows
+    if (rows.length === 0) {
+      res.code(404)
+        .send(`User with id ${req.params.id} not found`)
+    }
+    res.send(rows)
+  })
+  app.post('/user', {
+    schema: {
+      body: S.object()
+        .prop('username', S.string().required())
+        .prop('password', S.string().required())
+    }
+  }, async (req, res) => {
+    const client = await app.pg.connect()
+    try {
+      const { rows } = await client.query('INSERT into users(username, password) VALUES ($1, $2) RETURNING *', [req.body.username, req.body.password])
+      client.release()
+      res.code(201)
+        .header('x-generated-id', rows[0].id)
+        .send(rows[0])
+    } catch (err) {
+      console.log(err)
+      res.code(400)
+    }
   })
 }
--- routes/foo/bar.test.js
+++ routes/foo/bar.test.js
@@ -3,7 +3,7 @@ import fastify from 'fastify'
 import fp from 'fastify-plugin'
 import app from '../../app.js'
 
-const test = t.test
+const { test } = t
 
 test('load inside a folder', async ({ is }) => {
   const server = fastify()
@@ -13,6 +13,30 @@ test('load inside a folder', async ({ is }) => {
 
   const res = await server.inject('/foo')
   is(res.body, 'foo bar')
+
+  await server.close()
+})
+
+test('access to database', async ({ is }) => {
+  const server = fastify()
+
+  // so we can access decorators
+  server.register(fp(app))
+
+  const res = await server.inject('/foo/user/1')
+  is(res.body, '[{"id":1,"username":"santosam72","password":"pass"}]')
+
+  await server.close()
+})
+
+test('insert to database', async ({ is }) => {
+  const server = fastify()
+
+  // so we can access decorators
+  server.register(fp(app))
+
+  const res = await server.inject({ method: 'POST', url: '/foo/user', payload: { username: 'santosam72', password: 'pass' } })
+  is(res.body, '[{"id":1,"username":"santosam72","password":"pass"}]')
 
   await server.close()
 })
